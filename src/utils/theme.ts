aa979e6f62328599ffec11354a0db6d4b4a78d13
maramzan <arsalanramzan811@gmail.com>
2024-05-03 20:01:21 +0500

    project setup

---
+++ src/utils/theme.ts
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#23A6F0",
+      light: "#737373",
+    },
+    secondary: {
+      main: "#23856D",
+      light: "#2DC071",
+    },
+    text: {
+      primary: "#252B42",
+      secondary: "#737373",
+    },
+    error: {
+      main: "#red",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat, sans-serif",
+  },
+});
+
+export default theme;
